5ae889183027c24c8bca47eb6f22260d218ce334
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-27 12:00:16 +0000

    added built in django authentication log in/log out and configured settings for auth urls

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,11 +2,22 @@ from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 
 from .forms import LoginForm
 
 
 # Create your views here.
+
+# Display Dashboard
+@login_required
+def dashboard(request):
+    template = 'account/dashboard.html'
+    context = {
+        'section': 'dashboard'
+    }
+    return render(request, template, context)
 
 
 # User Authentication and Login
