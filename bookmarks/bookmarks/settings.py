d309cf4a4b398234af94d8f8d6eab081ba1c0f64
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-28 17:24:42 +0000

    built image model and manytomany relationship

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
     #third parties
     'social_django',
     'django_extensions',
