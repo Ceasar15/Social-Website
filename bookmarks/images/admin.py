06a5d3f65ec9c7fb75564647f4234b1357001a56
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-28 17:48:09 +0000

    cleaning form fields

--- bookmarks/images/admin.py
+++ bookmarks/images/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.admin.options import ModelAdmin
+from .models import Image
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'images', 'created']
+    list_filter = ['created']
+
+
+
+admin.site.register(Image,ImageAdmin)
--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils.text import slugify
 
 # Create your models here.
-class Images(models.Model):
+class Image(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title       = models.CharField(max_length=200)
     slug        = models.SlugField(max_length=200, blank=True)
