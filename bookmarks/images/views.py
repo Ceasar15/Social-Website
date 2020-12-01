62ebbdbd7dcc845fad8482c6e3b0378cb17db331
Ceasar15 <ceasarkwadwo@gmail.com>
2020-12-01 20:50:21 +0000

    added something

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -52,5 +52,53 @@ def image_detail(request, id, slug):
 
 
 
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+        'images/image/detail.html',
+        {'section': 'images',
+        'image': image})
+
+
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+        'images/image/detail.html',
+        {'section': 'images',
+        'image': image})
+
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+        'images/image/detail.html',
+        {'section': 'images',
+        'image': image})
+
+
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+        'images/image/detail.html',
+        {'section': 'images',
+        'image': image})
+
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+        'images/image/detail.html',
+        {'section': 'images',
+        'image': image})
+
+
+
+
 
 
