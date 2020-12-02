ff96c27a1aa2e70e13a6efecb2205e0682afac98
Ceasar15 <ceasarkwadwo@gmail.com>
2020-12-02 17:10:10 +0000

    performing ajax requests

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 from . import views
 
@@ -6,5 +7,5 @@ app_name = 'images'
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like')
 ]
-    
